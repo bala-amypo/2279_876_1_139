bc53bb4234d3c5611c875a1da03555ebb5185fc6
bala-amypo <bala@amypo.in>
2025-12-19 09:39:21 +0000

    User commit

--- src/main/java/com/example/demo/repository/TransferEvaluationResultRepository.java
+++ src/main/java/com/example/demo/repository/TransferEvaluationResultRepository.java
@@ -1,13 +1,8 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.entity.TransferEvaluationResult;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransferEvaluationResultRepository
         extends JpaRepository<TransferEvaluationResult, Long> {
-
-    List<TransferEvaluationResult> findBySourceCourseId(Long courseId);
 }
