0a4eaa64f9284c8f3d293c8a646cb5df5fdbed05
bala-amypo <bala@amypo.in>
2025-12-19 05:43:11 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/University.java
@@ -0,0 +1,73 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "universities", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "name")
+})
+public class University {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String accreditationLevel;
+
+    private String country;
+
+    private Boolean active = true;
+
+    public University() {
+    }
+
+    public University(String name, String accreditationLevel, String country) {
+        this.name = name;
+        this.accreditationLevel = accreditationLevel;
+        this.country = country;
+        this.active = true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccreditationLevel() {
+        return accreditationLevel;
+    }
+
+    public void setAccreditationLevel(String accreditationLevel) {
+        this.accreditationLevel = accreditationLevel;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
