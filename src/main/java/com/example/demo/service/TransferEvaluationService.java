07f9e6974454c362e8a5cfe3412b92d7f3906f13
bala-amypo <bala@amypo.in>
2025-12-19 10:12:07 +0000

    User commit

--- src/main/java/com/example/demo/service/TransferEvaluationService.java
+++ src/main/java/com/example/demo/service/TransferEvaluationService.java
@@ -3,8 +3,6 @@ package com.example.demo.service;
 import com.example.demo.entity.TransferEvaluation;
 
 public interface TransferEvaluationService {
-
     String evaluateTransfer(Long studentId, Long courseId);
-
     TransferEvaluation getEvaluationById(Long evaluationId);
 }
