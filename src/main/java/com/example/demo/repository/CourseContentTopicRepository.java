423caebc6cbd0e5736b5f73144daf4bbe51d29c3
bala-amypo <bala@amypo.in>
2025-12-19 06:08:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/CourseContentTopicRepository.java
+++ src/main/java/com/example/demo/repository/CourseContentTopicRepository.java
@@ -1,37 +1,4 @@
-package com.example.demo.repository;
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.University;
-
-public interface UniversityRepository extends JpaRepository<University, Long> {
-
-    Optional<University> findByName(String name);
-}
-
-courseRepo
-package com.example.demo.repository;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-
-    Optional<Course> findByUniversityIdAndCourseCode(Long universityId, String courseCode);
-
-    List<Course> findByUniversityIdAndActiveTrue(Long universityId);
-}
-
-
-
-
-CourseContent
 
 package com.example.demo.repository;
 
