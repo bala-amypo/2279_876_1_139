89c24ab7742fdf29e8cce5ed77ee350149d1d0a0
bala-amypo <bala@amypo.in>
2025-12-24 04:29:13 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,68 +1,10 @@
-// // package com.example.demo.config;
-
-// // import io.swagger.v3.oas.models.OpenAPI;
-// // import io.swagger.v3.oas.models.Components;
-// // import io.swagger.v3.oas.models.info.Info;
-// // import io.swagger.v3.oas.models.security.SecurityRequirement;
-// // import io.swagger.v3.oas.models.security.SecurityScheme;
-// // import io.swagger.v3.oas.models.servers.Server;
-// // import org.springframework.context.annotation.Bean;
-// // import org.springframework.context.annotation.Configuration;
-
-// // import java.util.List;
-
-// // @Configuration
-// // public class OpenApiConfig {
-
-// //     @Bean
-// //     public OpenAPI customOpenAPI() {
-
-// //         return new OpenAPI()
-// //                 .info(new Info()
-// //                         .title("University Course Transfer Validator API")
-// //                         .version("1.0"))
-// //                 .servers(List.of(
-// //                         new Server().url("")
-// //                 ))
-// //                 .addSecurityItem(
-// //                         new SecurityRequirement().addList("bearerAuth")
-// //                 )
-// //                 .components(
-// //                         new Components().addSecuritySchemes(
-// //                                 "bearerAuth",
-// //                                 new SecurityScheme()
-// //                                         .type(SecurityScheme.Type.HTTP)
-// //                                         .scheme("bearer")
-// //                                         .bearerFormat("JWT")
-// //                         )
-// //                 );
-// //     }
-// // }
-// package com.example.demo.config;
-
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.info.Info;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-
-// @Configuration
-// public class OpenApiConfig {
-
-//     @Bean
-//     public OpenAPI customOpenAPI() {
-//         return new OpenAPI()
-//                 .info(new Info()
-//                         .title("University Course Transfer Validator API")
-//                         .version("1.0")
-//                         .description("API documentation"));
-//     }
-// }
-
-
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -70,18 +12,29 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
+public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
         return new OpenAPI()
                 .info(new Info()
-                        .title("Supplier Diversity Tracker")
-                        .version("1.0")
-                        .description("API documentation for AmyPO test cases")
-                )
+                        .title("University Course Transfer Validator API")
+                        .version("1.0"))
                 .servers(List.of(
-                        new Server().url("")
-                ));
+                        new Server().url("https://9168.32procr.amypo.ai/")
+                ))
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                )
+                .components(
+                        new Components().addSecuritySchemes(
+                                "bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
