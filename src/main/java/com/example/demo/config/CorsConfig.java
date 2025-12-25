a4637cd7efbf0952a2fc7b717e52beac9f059df0
bala-amypo <bala@amypo.in>
2025-12-25 14:34:05 +0000

    User commit

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -1,18 +1,19 @@
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
+// @Configuration
+// public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedHeaders("*");
-    }
-}
+//         registry.addMapping("/**")
+//                 .allowedOrigins("*")
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+//                 .allowedHeaders("*");
+//     }
+// }
+//cors error
